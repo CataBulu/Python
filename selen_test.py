161b9a5325a7cd67b670628b16cfa8ffd56c1c1f
CataBulu <bulugea.catalin1@gmail.com>
2026-03-18 21:43:19 +0200

    selenium test

---
+++ selen_test.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.edge.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+edge_options = Options()
+edge_options.add_experimental_option("detach", True)
+
+driver = webdriver.Edge(options=edge_options)
+driver.implicitly_wait(10)
+
+# 1. Maximizăm fereastra imediat ce pornește browserul
+driver.maximize_window()
+
+driver.get("https://ro.wikipedia.org/")
+
+# 2. Folosim ID-ul exact al barei de căutare în loc de NAME
+search_box = driver.find_element(By.ID, "searchInput")
+search_box.send_keys("Buna Ziua")
+search_box.send_keys(Keys.RETURN)
